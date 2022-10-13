e859b7d83513c49582d3d793ed6a16e309d2e5ff
Adimea <ugochiazide7@gmail.com>
2022-10-13 14:50:33 -0700

    work this time

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - entry
+ * Return: Always 0
+ **/
 int main(void)
 {
-	int i;
-	double d;
-	char c;
-
-	printf("the size of an int is: %lu.\n", (unsigned long)sizeof(i));
-	printf("the size of an double is: %lu.\n", (unsigned long)sizeof(d));
-	printf("the size of an c is: %lu.\n", (unsigned long)sizeof(c));
-
+	printf("Size of a char: i% byte(s)\n", sizeof(char)); 
+	printf("Size of an int: i% byte(s)\n", sizeof(int));
+	printf("Size of a long int: i% byte(s)\n", sizeof(long int));
+	printf("Size of long long int: i% byte(s)\n", sizeof(long long int));
+	printf("Size of a float: i% byte(s)\n", sizeof(float));
 	return (0);
+}
