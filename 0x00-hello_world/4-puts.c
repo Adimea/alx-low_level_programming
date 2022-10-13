9b48b4b0f24eb989630bbc85ed3677dd3e7c8fe3
Adimea <ugochiazide7@gmail.com>
2022-10-13 12:32:01 -0700

    work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- * main - Entry
- * Return: always 0
- * Decription: c learning
+ *main - entry point
+ *
+ *Return: Always 0
  */
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
