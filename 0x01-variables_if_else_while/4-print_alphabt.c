e18a47bd944e90b0d00353b5d64362ff2882f1b0
Adimea <ugochiazide7@gmail.com>
2022-10-15 10:33:10 -0700

    task 4' help

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main -Entry point
  *
  * Return: Always 0 (success)
  */
@@ -11,11 +11,10 @@ int main(void)
 
 	c = 'a';
 	while
-		(c < 'z') {
+		(c <= 'z') {
 			if ((c != 'q' && c != 'e') && c <= 'z')
-
-					putchar(c);
-				c++;
+				putchar(c);
+			c++;
 		}
 	putchar('\n');
 	return (0);
