756ee59e9dd0b80d1e0e70ff6a976ccd4c2a2a10
Adimea <ugochiazide7@gmail.com>
2022-10-19 15:55:07 -0700

    print time tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,7 @@
  * starting with 0.
  * @n: The value of the times table to be printed.
  *
- * Description: is written by Honeydropjahbless
+ * Description: is written by Adimea
  */
 
 void print_times_table(int n)
