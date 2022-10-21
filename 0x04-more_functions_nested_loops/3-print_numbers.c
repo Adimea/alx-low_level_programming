a964101a8e051a576a556b83c2f22402749cf866
Adimea <ugochiazide7@gmail.com>
2022-10-21 12:22:28 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * print_mumbers - print from 0 to 9
- * Description - prints numbers 0 to 9
+ * print_numbers - print from 0 to 9
  * Return: Always 0
  */
-
 void print_numbers(void)
 {
 	int a = 0;
