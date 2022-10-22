04cd14ff3fdab2ad5977037602063069b8fef917
Adimea <ugochiazide7@gmail.com>
2022-10-21 21:23:17 -0700

    commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -21,7 +21,7 @@ void print_triangle(int size)
 	{
 		for (row = 1; row <= size; row++)
 		{
-			for (spaces = size - row; space >= 1; space--)
+			for (spaces = size - row; spaces >= 1; spaces--)
 			{
 				_putchar(' ');
 			}
