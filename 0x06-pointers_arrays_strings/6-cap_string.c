509c64ededdf69959916fbb3dd9a46b2e405e388
Adimea <ugochiazide7@gmail.com>
2022-10-27 08:57:55 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes
+ * cap_string - capitalizes words
  * @str: string
  * Return: pointer to string
  */
@@ -14,19 +14,19 @@ char *cap_string(char *str)
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 		if (str[i - 1] == ' ' ||
-		str[i - 1] == '\t' ||
-		str[i - 1] == '\n' ||
-		str[i - 1] == ',' ||
-		str[i - 1] == ';' ||
-		str[i - 1] == '.' ||
-		str[i - 1] == '!' ||
-		str[i - 1] == '?' ||
-		str[i - 1] == '"' ||
-		str[i - 1] == '(' ||
-		str[i - 1] == ')' ||
-		str[i - 1] == '{' ||
-		str[i - 1] == '}' ||
-	i == 0)
+		   str[i - 1] == '\t' ||
+		   str[i - 1] == '\n' ||
+		   str[i - 1] == ',' ||
+		   str[i - 1] == ';' ||
+		   str[i - 1] == '.' ||
+		   str[i - 1] == '!' ||
+		   str[i - 1] == '?' ||
+		   str[i - 1] == '"' ||
+		   str[i - 1] == '(' ||
+		   str[i - 1] == ')' ||
+		   str[i - 1] == '{' ||
+		   str[i - 1] == '}' ||
+		   i == 0)
 			str[i] -= 32;
 		i++;
 	}
