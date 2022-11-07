c52a016d9c7c8b6b8ab0808c95f958423940e4db
Adimea <ugochiazide7@gmail.com>
2022-11-07 12:27:14 -0800

    argstostr

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,5 +7,7 @@ char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
+ar *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
